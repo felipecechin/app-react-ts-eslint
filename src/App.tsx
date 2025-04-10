7c8a2805e1d721a2ab1f0499acfb569b4c579423
Felipe Cechin <ficechin@hotmail.com>
2025-04-10 17:37:00 -0300

    Using tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ function App() {
           />
         </a>
       </div>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       <h1>Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount(count => count + 1)}>
